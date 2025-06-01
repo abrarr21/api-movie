cc2984b5eaad1235165e441824e37f21a26aac8f
Abrar <abrarrr2110@gmail.com>
2025-06-01 16:30:08 +0530

    Customize JSON

--- internals/data/runtime.go
+++ internals/data/runtime.go
@@ -1,9 +1,14 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+// Unmarshaler shows this error if we're unable to parse the json string.
+var ErrInvalidRuntimeFormat = errors.New("Invalid Runtime format")
 
 // Declare a custom Runtime type, which has the underlying type int32 (the same as our Movie struct field).
 type Runtime int32
@@ -15,4 +20,26 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONvalue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONvalue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unqoutedJsonValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unqoutedJsonValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
